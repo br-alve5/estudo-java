7f3897714f5ce104c52f4a0e2b44eca560300c71
br-alve5 <bralves.07.96@gmail.com>
2025-02-26 12:39:55 -0300

    acrescentando exemplos dos tipos de variaveis

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 1500;
+
+        short numeroCurto = 1;
+        int numeroInteiro = numeroCurto;
+        short numeroCurto2 = (short) numeroInteiro;
+
+        int numero = 1;
+
+        numero = 2;
+
+        System.out.println(numero);
+
+        double valor_de_pi = 3.14;
+        valor_de_pi = 10.41;
+        System.out.println(valor_de_pi);
+
+        final double VALOR_DE_PI = 3.14;
+        // VALOR_DE_PI = 14.24; - não pode ser alterado.
+        System.out.println(VALOR_DE_PI);
+
     }
 }
