bc9e3f96d655360c4a90ec3acee9b279e86bea33
br-alve5 <bralves.07.96@gmail.com>
2025-03-04 18:05:21 -0300

    exemplo de escopo para documentação

---
+++ tags/src/Calculadora.java
@@ -0,0 +1,22 @@
+    /**
+    * <h1>Calculadora</h1>
+    * A Calculadora realiza operaçoes matematicas entre numeros inteiros
+    * <p>
+    * <b>Note:</b> Leia atentamente a documentaçao desta classe
+    * para desfrutar dos recursos oferecidos pelo autor
+    *
+    * @author Bruno R Alves
+    * @version 1.0
+    * @since 04/03/2025
+    */
+public class Calculadora {
+    /**
+     * Este metodo é utilizado para somar dois numeros inteiros
+     * @param numeroUm este é o primeiro parametro do metodo
+     * @param numeroDois este é o segundo parametro do metodo
+     * @return int o resultado deste metodo é a soma do dois numeros.
+    */
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+}
