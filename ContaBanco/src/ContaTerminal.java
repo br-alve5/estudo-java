d4de1798aaa568ff62c71f7806ef63a4f5c91569
br-alve5 <bralves.07.96@gmail.com>
2025-03-05 19:19:46 -0300

    finalizado o desafio CONTA BANCO da trilha de java basico pela DIO

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        /*
+         * int numero
+         * String nomeCliente
+         * String agencia
+         * double saldo
+        */
+
+        // fornecer o nome
+        System.out.println("Digite seu nome:");
+        String nomeCliente = scanner.next();
+        
+        // fornecer o numero da conta
+        System.out.println("Digite o numero da conta:");
+        int numero = scanner.nextInt();
+
+        // fornecer o numero da agencia
+        System.out.println("Digite a agencia:");
+        String agencia = scanner.next();
+        
+        // fornecer o valor do saldo
+        System.out.println("Digite o valor de deposito:");
+        double saldo = scanner.nextDouble();
+
+        // imprimir informaçoes
+        System.out.println("Olá " + nomeCliente+", obrigado por criar uma conta em nosso banco, sua agencia é "
+        + agencia + " conta " + numero + " e seu saldo de R$" + saldo + " ja está disponivel para saque.");
+
     }
 }
